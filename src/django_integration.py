4329691298b9eee678782a69b069520759dedf7d
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-17 06:38:44 +0200

    A Better log cache cleaner

--- src/django_integration.py
+++ src/django_integration.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
 # Libraries
-from datetime import datetime,timedelta
 import utils
+from datetime import datetime
 
 # Import django model
 from Instanssi.screenshow.models import IRCMessage
 
 def django_log_cleanup():
-    filterdate = datetime.now() - timedelta(days=1)
-    IRCMessage.objects.filter(date__lt=filterdate).delete()
+    limit = 50
+    n = 0
+    last_id = 0
+    for msg in IRCMessage.objects.all().order_by('-id'):
+        last_id = msg.id
+        if n >= limit:
+            break;
+        n += 1
+    
+    IRCMessage.objects.filter(id__lt=last_id).delete()
     
 def django_log_add(user, msg, event_id):
     try:
