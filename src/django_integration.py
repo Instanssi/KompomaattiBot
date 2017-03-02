84ad38d520a3a5447ba661f96d2611da86c8045d
Tuomas Virtanen <katajakasa@gmail.com>
2017-03-03 01:23:56 +0200

    Improvements.

--- src/django_integration.py
+++ src/django_integration.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Libraries
-import utils
 from datetime import datetime
 
 # Django stuff
@@ -9,14 +8,17 @@ from Instanssi.kompomaatti.misc.events import get_upcoming
 from Instanssi.screenshow.models import IRCMessage
 from Instanssi.kompomaatti.models import Event
 
-def django_get_event(id):
+
+def django_get_event(event_id):
     try:
-        return Event.objects.get(pk=id)
-    except:
+        return Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
         return None
+
 
 def django_get_upcoming(event):
     return get_upcoming(event)[:5]
+
 
 def django_log_cleanup():
     limit = 50
@@ -25,18 +27,19 @@ def django_log_cleanup():
     for msg in IRCMessage.objects.all().order_by('-id'):
         last_id = msg.id
         if n >= limit:
-            break;
+            break
         n += 1
     
     IRCMessage.objects.filter(id__lt=last_id).delete()
-    
+
+
 def django_log_add(user, msg, event_id):
     try:
         message = IRCMessage()
         message.event_id = event_id
         message.date = datetime.now()
-        message.message = unicode(utils.decode(msg))
-        message.nick = unicode(utils.decode(user))
+        message.message = msg
+        message.nick = user
         message.save()
     except UnicodeDecodeError:
         return False
--- src/main.py
+++ src/main.py
@@ -1,32 +1,76 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import django
+import irc3
+from .django_integration import django_log_add, django_log_cleanup
+
 # Attempt to find configuration
 try:
     import config
-except:
-    print "Config module not found! Remember to rename config.py-dist to config.py!"
+except ImportError:
+    print("Config module not found! Remember to rename config.py-dist to config.py!")
     exit()
-    
-# Library imports
-import os,sys
-import django
 
 # Django environment
 sys.path.append(config.DJANGO_ROOT)
 os.environ['DJANGO_SETTINGS_MODULE'] = config.DJANGO_SETTINGS
-django.setup()
 
-# Other imports
-from bot import LogBotFactory, BotException
-from twisted.internet import reactor
 
-# Main
+@irc3.plugin
+class KompomaattiBot:
+    requires = [
+        'irc3.plugins.core',
+        'irc3.plugins.command',
+        'irc3.plugins.human',
+    ]
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.log = self.bot.log
+        self.channel = None
+
+    def connection_made(self):
+        self.log.info("Connected")
+
+    def server_ready(self):
+        self.log.info("Server ready")
+
+    def connection_lost(self):
+        self.log.info("Connection lost")
+
+    @irc3.event(irc3.rfc.JOIN)
+    def joined(self, mask, channel, **kw):
+        self.log.info("Joined channel '%s'", channel)
+        self.channel = channel
+
+    @irc3.event(irc3.rfc.PRIVMSG)
+    def privmsg(self, mask=None, data=None, **kw):
+        nick = mask.nick
+        target = kw.get('target')
+        self.log.info("%s @ %s: '%s'", nick, target, data)
+        if target == self.channel:
+            django_log_cleanup()
+            django_log_add(nick, data, config.EVENT_ID)
+
+
+def main():
+    bot_conf = dict(
+        nick=config.NICK,
+        autojoins=[config.CHANNEL],
+        host=config.SERVER, port=config.PORT, ssl=False,
+        includes=[
+            'irc3.plugins.core',
+            'irc3.plugins.command',
+            'irc3.plugins.human',
+            __name__,
+        ]
+    )
+    bot = irc3.IrcBot.from_config(bot_conf)
+    bot.run(forever=True)
+
+
 if __name__ == '__main__':
-    try:
-        botfactory = LogBotFactory(config.CHANNEL, config.NICK, config.SERVER, config.PORT, config.EVENT_ID)
-    except BotException as ex:
-        print "Exception:",ex.value
-        exit()
-        
-    reactor.connectTCP(config.SERVER, config.PORT, botfactory)
-    reactor.run()
+    django.setup()
+    main()
